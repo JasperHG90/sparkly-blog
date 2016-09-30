533995fc4de1910b6cb8c17efd22e8f831bcfc55
Jasper Ginn <jasperginn@gmail.com>
2016-09-30 11:44:57 +0000

    Initial commit

---
+++ main.R
@@ -0,0 +1,5 @@
+#
+# Connect to spark and do some analyses
+# Jasper Ginn
+# 30/09/2016
+#
---
+++ settings/installRpackages.R
@@ -0,0 +1,7 @@
+#
+# Installing R packages. Only have to do this once because packrat restores lib. 
+# Jasper Ginn
+# 30/09/2016
+#
+
+install.packages(c("tidyverse", "tidyjson", "sparklyr"))
